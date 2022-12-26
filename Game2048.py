cd17a174a4f8e42a0c0bae679922880e9ac26eef
Phuc Nguyen <66092633+Saotalangim161099@users.noreply.github.com>
2022-12-26 12:51:11 -0500

    Week 1- Merge method Finalized

--- Game2048.py
+++ Game2048.py
@@ -1,24 +1,20 @@
-import main
-
-
-class Game2048:
-    def __init__(self, board):
-        self.board = board
-
-    def print(self):
-        for i in self.nums:
-            print(i, end=" ")
-
-    def merge(self, nums):
+def merge(nums):
+    """
+    Function that merges a single row or column in 2048.
+    """
+    # If the line is with length 1, then we cannot merge it. Return the given line.
+    if (len(nums) == 1):
+        return nums
+    else:
         # prev is used to compare, while next_ is used to traverse the list
         prev = None
         result_list = []
+
         for next_ in nums:
-            # print("prev: " + str(prev) + "\n")
-            # print("Next: " + str(next_) + " ")
             # if next_ is not 0, skip the loop
             if not next_:
                 continue
+            # if there is no prev saved, then set prev to the current next considered
             if prev is None:
                 prev = next_
             elif prev == next_:
@@ -36,9 +32,45 @@ class Game2048:
         return result_list
 
 
-if __name__ == '__main__':
-    list = [8, 16, 16, 8]
-    game_play = Game2048(list)
-    # game_play.print()
-    for i in game_play.merge(game_play.nums):
-        print(i, end=" ")
+def merge_test(test):
+    """
+    Tests whether the merge function passes the given input/ output case.
+    Accepts a tuple with 2 values: (input_line, expected_output_line)
+    """
+    input_line, expected_output_line = test
+    if merge(input_line) == expected_output_line:
+        return 'PASSED'
+    else:
+        return 'FAILED'
+
+
+class MergeTestSuite:
+    def __init__(self, input_tests):
+        self.tests = input_tests
+
+    def run_tests(self):
+        """
+        Run all the tests in the test suite for the merge function.
+        """
+        for index, test in enumerate(self.tests):
+            print('Test ', str(index + 1) + ': ', str(test[0]), '-->', str(test[1]), '-', merge_test(test))
+
+    def add_test(self, test):
+        """
+        Adds a test to the test suite.
+        :param test:
+        :return:
+        """
+        self.tests.append(test)
+
+
+tests = [
+    ([2, 0, 2, 4], [4, 4, 0, 0]),
+    ([0, 0, 2, 2], [4, 0, 0, 0]),
+    ([2, 2, 0, 0], [4, 0, 0, 0]),
+    ([2, 2, 2, 2, 2], [4, 4, 2, 0, 0]),
+    ([8, 16, 16, 8], [8, 32, 8, 0]),
+]
+
+merge_test_suite = MergeTestSuite(tests)
+merge_test_suite.run_tests()
